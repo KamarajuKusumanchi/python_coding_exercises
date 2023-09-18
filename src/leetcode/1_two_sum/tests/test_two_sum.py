a1d1878f0cee3a3746cab971cb2763f056b0db53
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2023-09-18 00:48:44 -0400

    lgtm

--- src/leetcode/1_two_sum/tests/test_two_sum.py
+++ src/leetcode/1_two_sum/tests/test_two_sum.py
@@ -10,7 +10,8 @@ from two_sum import Solution
         ([3, 2, 4], 6, [1, 2]),
         ([3, 3], 6, [0, 1]),
         ([3, 5, 1, 4, -8], 5, [2, 3]),
-        ([3,4,9,6,4], 8, [1,4])
+        ([3,4,9,6,4], 8, [1,4]),
+        ([4,-2,5,0,6,3,2,7], 1, [1,5])
     ],
 )
 def test_two_sum(nums: List[int], target: int, expected_result: List[int]) -> None:
